f09c61096092712238d15d6c52e8d50bbb3d9a76
Syed Hassan Raza <hassanraza@satjapan.co>
2025-10-14 18:58:59 +0500

    updates

--- resources/views/components/dashboard/dashboard-maincontent.blade.php
+++ resources/views/components/dashboard/dashboard-maincontent.blade.php
@@ -1,106 +1,233 @@
 @php
-    $Data = [[], [], [], [], []];
+    $Data = [[], [], [], []];
 @endphp
 
 <div class="main-section-dashboard">
 
-    <div class="page-title-cmn">
+    <div class="page-title-cmn d-md-block d-none">
         <h1 class="pagepara">
             Dashboard
         </h1>
     </div>
 
-    <div class="search-dash">
-        <div class="title-para-search">
-            <h2 class="tile-srch">Good Afternoon Dev</h2>
-            <p class="tile-para">Here's what's happening with your site today.</p>
-        </div>
-        <div class="dropdowns-elemets">
-            <div class="dropdown">
-                <button class="dropdown-toggle mn" type="button" id="drp_month" data-bs-toggle="dropdown"
-                    aria-expanded="false">
-                    March
-                </button>
-                <ul class="dropdown-menu" id="select_month">
-                    <li class="dropdown-item">April</li>
-                    <li class="dropdown-item">May</li>
-                    <li class="dropdown-item">Jun</li>
-                    <li class="dropdown-item">July</li>
-                </ul>
+    <div class="progress-card-section">
+        <div class="row g-3">
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <a href="" class="text-decoration-none">
+                    <div class="progress-card shadow-sm">
+                        <div class="d-flex align-items-center justify-content-between mb-3">
+                            <h3 class="mb-0">Orders Created</h3>
+                            <img src="{{ asset('assets/images/icons/order-created.png') }}" height="34"
+                                width="34" loading="lazy" alt="icons">
+                        </div>
+                        <p>142</p>
+                        <div class="count-progress">
+                            <svg width="20" height="20" class="rise">
+                                <use href="#dashboard_arrow"></use>
+                            </svg>
+                            <div class="progress-det">
+                                <span class="me-2 rise">10%</span>
+                                <span>Last Month</span>
+                            </div>
+                        </div>
+                    </div>
+                </a>
             </div>
-            <div class="dropdown">
-                <button class="dropdown-toggle yr" type="button" id="drp_year" data-bs-toggle="dropdown"
-                    aria-expanded="false">
-                    2026
-                </button>
-                <ul class="dropdown-menu" id="select_year">
-                    <li class="dropdown-item">2025</li>
-                    <li class="dropdown-item">2024</li>
-                    <li class="dropdown-item">2023</li>
-                    <li class="dropdown-item">2022</li>
-                </ul>
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <a href="" class="text-decoration-none">
+                    <div class="progress-card shadow-sm">
+                        <div class="d-flex align-items-center justify-content-between mb-3">
+                            <h3 class="mb-0">Price Quotes Pending (Used Cars)</h3>
+                            <img src="{{ asset('assets/images/icons/price-quote-usedcar.png') }}" height="34"
+                                width="34" loading="lazy" alt="icons">
+                        </div>
+                        <p>3</p>
+                        <div class="count-progress">
+                            <svg width="20" height="20" class="rise">
+                                <use href="#dashboard_arrow"></use>
+                            </svg>
+                            <div class="progress-det">
+                                <span class="me-2 rise">3%</span>
+                                <span>Last Month</span>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <a href="" class="text-decoration-none">
+                    <div class="progress-card shadow-sm">
+                        <div class="d-flex align-items-center justify-content-between mb-3">
+                            <h3 class="mb-0">Price Quotes Pending (New Cars)</h3>
+                            <img src="{{ asset('assets/images/icons/price-quote-newcar.png') }}" height="34"
+                                width="34" loading="lazy" alt="icons">
+                        </div>
+                        <p>14</p>
+                        <div class="count-progress">
+                            <svg width="20" height="20" class="downfall">
+                                <use href="#dashboard_arrow"></use>
+                            </svg>
+                            <div class="progress-det">
+                                <span class="me-2 downfall">-10%</span>
+                                <span>Last Month</span>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <a href="" class="text-decoration-none">
+                    <div class="progress-card shadow-sm">
+                        <div class="d-flex align-items-center justify-content-between mb-3">
+                            <h3 class="mb-0">User Inquiries Today</h3>
+                            <img src="{{ asset('assets/images/icons/user-inquiry.png') }}" height="34" width="34"
+                                loading="lazy" alt="icons">
+                        </div>
+                        <p>25</p>
+                        <div class="count-progress">
+                            <svg width="20" height="20" class="rise">
+                                <use href="#dashboard_arrow"></use>
+                            </svg>
+                            <div class="progress-det">
+                                <span class="me-2 rise">20%</span>
+                                <span>Last Month</span>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <a href="" class="text-decoration-none">
+                    <div class="progress-card shadow-sm">
+                        <div class="d-flex align-items-center justify-content-between mb-3">
+                            <h3 class="mb-0">Reservations Pending</h3>
+                            <img src="{{ asset('assets/images/icons/reservation-pending.png') }}" height="34"
+                                width="34" loading="lazy" alt="icons">
+                        </div>
+                        <p>25</p>
+                        <div class="count-progress">
+                            <svg width="20" height="20" class="rise">
+                                <use href="#dashboard_arrow"></use>
+                            </svg>
+                            <div class="progress-det">
+                                <span class="me-2 rise">20%</span>
+                                <span>Last Month</span>
+                            </div>
+                        </div>
+                    </div>
+                </a>
             </div>
         </div>
     </div>
 
-    <div class="sale-numbers">
-        @foreach ($Data as $item)
-            <div class="sale-numbers-item">
-                <span></span>
-                <div class="count-tainer">
-                    <h3>Total Vehicles</h3>
-                    <p>$148.40</p>
+    <div class="sale-numbers mt-0">
+        <div class="row g-3">
+            <div class="col-12">
+                <div class="d-flex align-items-center gap-2 mb-3">
+                    <img src="{{ asset('assets/images/icons/vehicle-statistics.png') }}" height="22" width="22"
+                        loading="lazy" alt="icon">
+                    <h2 class="fs-6 mb-0">Vehicle Statistics</h2>
                 </div>
             </div>
-        @endforeach
+            @foreach ($Data as $item)
+                <div class="col-lg-3 col-md-4 col-sm-12">
+                    <div class="sale-numbers-item">
+                        <span></span>
+                        <div class="count-tainer">
+                            <h3>Total Vehicles</h3>
+                            <p>$148.40</p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
 
     <div class="progress-card-section">
         <div class="row g-3">
-            <div class="col-lg-4 col-md-6 col-sm-12">
-                <div class="progress-card shadow-sm">
-                    <h3>Unread Mails</h3>
-                    <p>$1,280</p>
-                    <div class="count-progress">
-                        <svg width="20" height="20" class="rise">
-                            <use href="#dashboard_arrow"></use>
-                        </svg>
-                        <div class="progress-det">
-                            <span class="me-2 rise">15%</span>
-                            <span>last mth</span>
+            <div class="col-lg-3 col-md-4 col-sm-12">
+                <a href="" class="text-decoration-none">
+                    <div class="progress-card shadow-sm">
+                        <div class="d-flex align-items-center justify-content-between mb-3">
+                            <h3 class="mb-0">Unread Mails</h3>
+                            <img src="{{ asset('assets/images/icons/unread-mails.png') }}" height="24"
+                                width="24" loading="lazy" alt="icons">
+                        </div>
+                        <p>142</p>
+                        <div class="count-progress">
+                            <svg width="20" height="20" class="rise">
+                                <use href="#dashboard_arrow"></use>
+                            </svg>
+                            <div class="progress-det">
+                                <span class="me-2 rise">10%</span>
+                                <span>Last Month</span>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </a>
             </div>
-            <div class="col-lg-4 col-md-6 col-sm-12">
-                <div class="progress-card shadow-sm">
-                    <h3>Today's Inquires</h3>
-                    <p>14</p>
-                    <div class="count-progress">
-                        <svg width="20" height="20" class="downfall">
-                            <use href="#dashboard_arrow"></use>
-                        </svg>
-                        <div class="progress-det">
-                            <span class="me-2 downfall">10%</span>
-                            <span>last mth</span>
+            <div class="col-lg-3 col-md-4 col-sm-12">
+                <a href="" class="text-decoration-none">
+                    <div class="progress-card shadow-sm">
+                        <div class="d-flex align-items-center justify-content-between mb-3">
+                            <h3 class="mb-0">Today's Auction</h3>
+                            <img src="{{ asset('assets/images/icons/today-auction.png') }}" height="24"
+                                width="24" loading="lazy" alt="icons">
+                        </div>
+                        <p>3</p>
+                        <div class="count-progress">
+                            <svg width="20" height="20" class="rise">
+                                <use href="#dashboard_arrow"></use>
+                            </svg>
+                            <div class="progress-det">
+                                <span class="me-2 rise">3%</span>
+                                <span>Last Month</span>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </a>
             </div>
-            <div class="col-lg-4 col-md-6 col-sm-12">
-                <div class="progress-card shadow-sm">
-                    <h3>Today's Auction</h3>
-                    <p>25</p>
-                    <div class="count-progress">
-                        <svg width="20" height="20" class="rise">
-                            <use href="#dashboard_arrow"></use>
-                        </svg>
-                        <div class="progress-det">
-                            <span class="me-2 rise">20%</span>
-                            <span>last mth</span>
+            <div class="col-lg-3 col-md-4 col-sm-12">
+                <a href="" class="text-decoration-none">
+                    <div class="progress-card shadow-sm">
+                        <div class="d-flex align-items-center justify-content-between mb-3">
+                            <h3 class="mb-0">Pending Orders</h3>
+                            <img src="{{ asset('assets/images/icons/pending-orders.png') }}" height="24"
+                                width="24" loading="lazy" alt="icons">
+                        </div>
+                        <p>14</p>
+                        <div class="count-progress">
+                            <svg width="20" height="20" class="downfall">
+                                <use href="#dashboard_arrow"></use>
+                            </svg>
+                            <div class="progress-det">
+                                <span class="me-2 downfall">-10%</span>
+                                <span>Last Month</span>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-12">
+                <a href="" class="text-decoration-none">
+                    <div class="progress-card shadow-sm">
+                        <div class="d-flex align-items-center justify-content-between mb-3">
+                            <h3 class="mb-0">Shipped Orders</h3>
+                            <img src="{{ asset('assets/images/icons/shipped-orders.png') }}" height="24"
+                                width="24" loading="lazy" alt="icons">
+                        </div>
+                        <p>25</p>
+                        <div class="count-progress">
+                            <svg width="20" height="20" class="rise">
+                                <use href="#dashboard_arrow"></use>
+                            </svg>
+                            <div class="progress-det">
+                                <span class="me-2 rise">20%</span>
+                                <span>Last Month</span>
+                            </div>
+                        </div>
+                    </div>
+                </a>
             </div>
         </div>
     </div>
